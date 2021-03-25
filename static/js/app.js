641ebc8b91d78bf254edcbab059d4ac6a908b0c7
Joe <josephmvillavicencio@gmail.com>
2021-03-25 14:21:30 -0700

    added title to bar chart and changed sizing

--- static/js/app.js
+++ static/js/app.js
@@ -4,87 +4,120 @@ $(document).ready(function () {
   });
   var data = JSON.parse(document.getElementById("traces").getAttribute("data"));
 
-
-
   var width = 970;
-  var height = 500;
+  var height = 350;
   var padding = 30;
   var margin = 135;
   var adj = 20;
   // we are appending SVG first
-  var svg = d3.select("#viz").append("svg")
+  var svg = d3
+    .select("#viz")
+    .append("svg")
     .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "-" + adj + " -" + adj + " " + (width + adj) + " " + (height + adj))
+    .attr(
+      "viewBox",
+      "-" + adj + " -" + adj + " " + (width + adj) + " " + (height + adj)
+    )
     .style("margin", "auto")
     .classed("svg-content", true);
 
   //-----------------------DATA PREPARATION------------------------//
   var dataset = data;
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, (d) => { return d.y })])
-    .range([0, height - padding])
-
-
-  svg.selectAll("rect")
+  const yScale = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(dataset, (d) => {
+        return d.y;
+      }),
+    ])
+    .range([0, height - padding]);
+// bars
+  svg
+    .selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => { return i * width / dataset.length + padding })
-    .attr("y", (d) => { return height - yScale(d.y) - padding })
-    .attr("height", (d) => { return yScale(d.y) })
+    .attr("x", (d, i) => {
+      return (i * width) / dataset.length + padding;
+    })
+    .attr("y", (d) => {
+      return height - yScale(d.y) - padding;
+    })
+    .attr("height", (d) => {
+      return yScale(d.y);
+    })
     .attr("width", width / dataset.length - (padding + margin))
     .attr("class", "bar")
     .append("title")
-    .text((d) => { return "name: " + d.x + "\n" + "messages sent: " + d.y })
-
-
-  svg.selectAll("text")
+    .text((d) => {
+      return "name: " + d.x + "\n" + "messages sent: " + d.y;
+    });
+// tool tip
+  svg
+    .selectAll("text")
     .data(dataset)
     .enter()
     .append("text")
-    .text((d) => { return d.x + ": \n" + d.y})
-    .attr("x", (d, i) => { return i * width / dataset.length + (width / dataset.length - (padding + margin)) / 2 - 15})
-    .attr("y", (d) => { return height  })
+    .text((d) => {
+      return d.x + ": \n" + d.y;
+    })
+    .attr("x", (d, i) => {
+      return (
+        (i * width) / dataset.length +
+        (width / dataset.length - (padding + margin)) / 2 -
+        15
+      );
+    })
+    .attr("y", (d) => {
+      return height;
+    })
+    .style("font-size", "1.25rem");
+
+  // title
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", -8)
+    .attr("text-anchor", "middle")
     .style("font-size", "16px")
-   
+    .style("text-decoration", "underline")
+    .text("total messages sent");
 
-  const yAxis = d3.scaleLinear()
-    .domain([0, d3.max(dataset, (d) => { return d.y })])
-    .range([height - padding, 0])
-
+  // x axis
+  const yAxis = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(dataset, (d) => {
+        return d.y;
+      }),
+    ])
+    .range([height - padding, 0]);
 
   svg
     .append("g")
-    .attr("transform", "translate(25, 0)")      // This controls the vertical position of the Axis
+    .attr("transform", "translate(25, 0)") // This controls the vertical position of the Axis
     .call(d3.axisLeft(yAxis));
 
+  var xScale = d3.scaleLinear().domain([]).range([25, 900]);
 
-    var xScale = d3.scaleLinear()
-    .domain([])
-    .range([25, 900]);
+  var xAxis = d3.axisBottom(xScale);
 
-var xAxis = d3.axisBottom(xScale);
-
-
-svg.append("g")
+  svg
+    .append("g")
     .attr("transform", "translate(0," + (height - padding) + ")")
     .call(xAxis);
-
-
-
-
-
 });
 
+// var config = { responsive: true }
+// var layout = {
+//   title: 'messages sent per user',
+//   font: {
+//     size: 17,
+//     family: 'Nunito, sans-serif'
 
-    // var config = { responsive: true }
-    // var layout = {
-    //   title: 'messages sent per user',
-    //   font: {
-    //     size: 17,
-    //     family: 'Nunito, sans-serif'
-
-    //   }
-    // };
-    // Plotly.newPlot('traces', data, layout, config);
-    // var data = JSON.parse(document.getElementById("traces").getAttribute("data"));
+//   }
+// };
+// Plotly.newPlot('traces', data, layout, config);
+// var data = JSON.parse(document.getElementById("traces").getAttribute("data"));
